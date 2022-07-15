8904ce69c58faf4392b92ecd3709e4653618e742
Sachin Lakshan <sachinlakshan04@gmail.com>
2022-07-15 21:20:45 +0530

    GET user by id route implemented.

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,9 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
-const users = [
-  {
-    firstName: "John",
-    lastName: "Doe",
-    age: 25,
-  },
-  {
-    firstName: "Jane",
-    lastName: "Doe",
-    age: 24,
-  },
-];
+const users = [];
 
 router.get("/", (req, res) => {
   res.send(users);
@@ -21,8 +11,15 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res) => {
   const user = req.body;
-  users.push(user);
+  users.push({ ...user, id: uuidv4() });
+
   res.send(`User with the name ${user.firstName} added to the database`);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const foundUser = users.find((user) => user.id === id);
+  res.send(foundUser);
 });
 
 export default router;
