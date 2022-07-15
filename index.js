c86e042be48fa0c2a8102bc0bb0d142a4ae8a7c0
Sachin Lakshan <sachinlakshan04@gmail.com>
2022-07-15 20:13:38 +0530

    GET request implemented for getting all the users

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+import usersRoutes from "./routes/users.js";
+
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
+app.use("/users", usersRoutes);
+
+app.get("/", (req, res) => {
+  console.log("TEST!");
+  res.send("Hello from HomePage");
+});
 
 app.listen(PORT, () =>
-  console.log(`Server running on port: http://localhost:${PORT}`)
+  console.log(`Server Running on port: http://localhost:${PORT}`)
 );
